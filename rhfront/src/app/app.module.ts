a739db5641406e729604ab1f61ed5d3dd69a1e7d
cealbemu1980 <cebemu1980@gmail.com>
2021-12-15 04:38:16 -0500

    uno
    uno

--- rhfront/src/app/app.module.ts
+++ rhfront/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './screen/login/login.component';
+import { JobComponent } from './screen/job/job.component';
+import { CountryComponent } from './screen/country/country.component';
+import { RegionComponent } from './screen/region/region.component';
+import { LocationComponent } from './screen/location/location.component';
+import { DepartamentComponent } from './screen/departament/departament.component';
+import { EmployeeComponent } from './screen/employee/employee.component';
+import { JobhistoryComponent } from './screen/jobhistory/jobhistory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    JobComponent,
+    CountryComponent,
+    RegionComponent,
+    LocationComponent,
+    DepartamentComponent,
+    EmployeeComponent,
+    JobhistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ rhfront/src/app/screen/jobhistory/jobhistory.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JobhistoryComponent } from './jobhistory.component';
+
+describe('JobhistoryComponent', () => {
+  let component: JobhistoryComponent;
+  let fixture: ComponentFixture<JobhistoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JobhistoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JobhistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
